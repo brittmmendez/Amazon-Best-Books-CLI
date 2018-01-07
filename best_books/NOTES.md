cb92cce96755cb29ba50f3a05d415cc7e07d76f2
Brittany Morris <brittmmendez@gmail.com>
2018-01-07 02:34:21 +0000

    created mock up of CLI and added NOTES.md

---
+++ best_books/NOTES.md
@@ -0,0 +1,27 @@
+Part of my New Years Resolution is to read at least one new book a month.  To make sure I find the best book and am successful with this resolution, I made a Gem that will list Amazon’s best-selling books and then give me more information on the book I’m interested in.  
+
+STRUCTURE:
+https://www.amazon.com/gp/new-releases/digital-text/ref=zg_bs_tab_t_bsnr
+
+https://www.amazon.com/charts/2017-12-31/mostread/nonfiction/
+
+User types  Best-Books
+CLI - Greats user and gives intro
+Puts list of top 20 books
+CLI- asks user for input number on which book they’d like more information on
+Puts summary of book: Title, author, rating, edition, price, URL Purchasing information, Summary
+CLI- Asks user if they’d like to see another book, the list again, or exit.
+If book - display book-> info/summary
+If list- list books
+	If exit - say goodbye
+
+
+QUESTIONS:
+What all does my book need?
+Title
+Author
+Rating
+edition
+Price
+URL Purchasing information (will use to get to summary)
+Summary
--- best_books/lib/best_books/cli.rb
+++ best_books/lib/best_books/cli.rb
@@ -1,11 +1,59 @@
 class BestBooks::CLI
-
   def call
-    puts "Hello!!"
-
+    introduction
+    list_books
+    pick_book
+    exit_cli
   end
 
+  def introduction
+    puts "- - - - - - - - - -Amazon Charts- - - - - - - - -"
+    puts "The Top 20 Most Sold & Most Read Books of the Week"
+    puts ""
+  end
 
+  def list_books
+    puts "1.  book 1"
+    puts "2.  book 2"
+    puts "3.  book 3"
+    puts "4.  book 4"
+    puts "5.  book 5"
+    puts "6.  book 6"
+    puts "7.  book 7"
+    puts ""
+  end
 
+  def pick_book
+    input =""
+    while input != "exit"
+      puts "Type the number for the category that you would like to see the top apps for, type 'list' to see the categories again, or exit:"
+      input=gets.strip.downcase
+
+      case input
+      when "1"
+        puts "book 1 is about xyz"
+      when "2"
+        puts "book 2 is about xyz"
+      when "3"
+        puts "book 3 is about xyz"
+      when "4"
+        puts "book 4 is about xyz"
+      when "5"
+        puts "book 5 is about xyz"
+      when "6"
+        puts "book 6 is about xyz"
+      when "7"
+        puts "book 7 is about xyz"
+      when "list"
+      list_books
+    else
+      puts "Invalid number, Type the number for the book that you would like to see more information for, type 'list' to see the books again, or exit:"
+      end
+    end
+  end
+
+      def exit_cli
+      puts "Thank you trying my Gem!  I hope you found a new book to read!"
+    end
 
 end
